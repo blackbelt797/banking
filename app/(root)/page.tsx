dba256a4c2659a032229c51aba4b5ca8b4bf9a79
Gregg Cline <ggcline82@gmail.com>
2024-06-24 18:09:07 -0700

    fixing file architecture missed a setp during set up.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import HeaderBox from '@/components/ui/HeaderBox';
 
 const Home = () => {
- return <div> Home </div>;
+ const loggedIn = { firstName: 'Gregg' };
+
+ return (
+  <section className="home">
+   <div className="home-content">
+    <header className="home-header">
+     <HeaderBox
+      type="greeting"
+      title="Welcome"
+      user={loggedIn?.firstName || 'Guest'}
+      subtext="Access and manage your account and transactions efficently"
+     />
+    </header>
+   </div>
+  </section>
+ );
 };
 
 export default Home;
